0311e21d84a4f2ece0bf2b78d831ebe5b98c8d29
JuliVazquez <julivazquez09@gmail.com>
2023-06-30 20:56:24 -0300

    rm3

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -1,1 +1,11 @@
- 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'timbeta',
+  password: 'A77aque99.',
+  port: 5432,
+});
+
+module.exports = pool;
